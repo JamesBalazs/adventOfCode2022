0acf856ac31463de84c17cabcc46e10126aeb779
James Balazs <j.c.balazs1@gmail.com>
2022-12-04 10:57:26 +0000

    remove redundant map

--- day01.exs
+++ day01.exs
@@ -3,7 +3,7 @@ contents |> String.split("\n\n", trim: true)
 |> Enum.map(fn(x) ->
   String.split(x, "\n", trim: true)
   |> Enum.map(&String.to_integer/1)
+  |> Enum.sum
 end)
-|> Enum.map(&Enum.sum/1)
 |> Enum.max
 |> IO.inspect # 65912
